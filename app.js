649023491a2639dc35c0e470f53f6813d78455eb
programmermohaimin <you@example.com>
2022-05-27 10:46:30 +0600

    add main app

---
+++ app.js
@@ -0,0 +1,7 @@
+let gitCli = 'programmer moh';
+console.log(gitCli);
+
+const name = document.getElementById('root', function () {
+    const now = 'btn';
+    const programmer = now.padStart(length.toString(programmer))
+})
