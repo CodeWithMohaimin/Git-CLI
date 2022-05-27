168663f021c2d45b3ccd8994c4fe43bff92ed413
programmermohaimin <you@example.com>
2022-05-27 10:52:20 +0600

    log

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let gitCli = 'programmer moh';
 console.log(gitCli);
 
@@ -6,13 +6,16 @@ const name = document.getElementById('root', function () {
     const now = 'btn';
     const programmer = now.padStart(length.toString(programmer))
 })
-=======
+
 const life = document.getElementById('programmer moh')
 var i = 'programmers'
 console.log(i);
  
 
+console.log(gitCli);
+
+
 
 
  
->>>>>>> 34f6337790699ed9f1c369dd3d9ce80b318477de
+
